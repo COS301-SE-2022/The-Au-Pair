a1999101e3d519ea44ef7cdb8ca140294d319a74
Daniel Azmanov <u20441135@tuks.co.za>
2022-05-11 10:34:17 +0200

    Final static add-activity page

--- libs/parent-add-activity/feature/src/lib/parent-add-activity.component.ts
+++ libs/parent-add-activity/feature/src/lib/parent-add-activity.component.ts
@@ -1,17 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'the-au-pair-parent-add-activity',
   templateUrl: './parent-add-activity.component.html',
   styleUrls: ['./parent-add-activity.component.scss'],
 })
-export class ParentAddActivityComponent implements OnInit {
-  constructor() {
-    console.log();
-  }
-
-  ngOnInit(): void {
-    console.log();
-    
-  }
-}
+export class ParentAddActivityComponent{}
