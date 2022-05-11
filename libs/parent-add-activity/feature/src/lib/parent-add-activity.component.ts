7827a008b0bdf0f26b95b73e351ad4cafa58c991
Daniel Azmanov <u20441135@tuks.co.za>
2022-05-11 15:08:43 +0200

    Added activity class and retrieve form input

--- libs/parent-add-activity/feature/src/lib/parent-add-activity.component.ts
+++ libs/parent-add-activity/feature/src/lib/parent-add-activity.component.ts
@@ -1,8 +1,93 @@
 import { Component} from '@angular/core';
+import { FormArray } from '@angular/forms';
 
 @Component({
   selector: 'the-au-pair-parent-add-activity',
   templateUrl: './parent-add-activity.component.html',
   styleUrls: ['./parent-add-activity.component.scss'],
 })
-export class ParentAddActivityComponent{}
+export class ParentAddActivityComponent{
+  //Activity object to hold the activity details
+  activityDetails = <Activity>{};
+
+  //Constructor
+  constructor() {
+    console.log();
+  }
+
+  //Populate the activityDetails object from form input
+  getActivityValues(val : any)
+  { 
+    console.log(val);
+    
+    this.activityDetails = new Activity(val.activityName, val.description, val.location, val.dayOfWeek, val.timeSlot, val.budget, val.childId);
+  }
+}
+
+class Activity
+{
+  //Private variables
+  private _activityName: string;
+  private _description: string;
+  private _location: string;
+  private _dayOfTheWeek: string;
+  private _timeSlot: string;
+  private _budget: number;
+  private _childId: string;
+
+  //Getters and setters
+  public get activityName(): string {
+    return this._activityName;
+  }
+  public set activityName(value: string) {
+    this._activityName = value;
+  }
+  public get description(): string {
+    return this._description;
+  }
+  public set description(value: string) {
+    this._description = value;
+  }
+  public get location(): string {
+    return this._location;
+  }
+  public set location(value: string) {
+    this._location = value;
+  }
+  public get dayOfTheWeek(): string {
+    return this._dayOfTheWeek;
+  }
+  public set dayOfTheWeek(value: string) {
+    this._dayOfTheWeek = value;
+  }
+  public get timeSlot(): string {
+    return this._timeSlot;
+  }
+  public set timeSlot(value: string) {
+    this._timeSlot = value;
+  }
+  public get budget(): number {
+    return this._budget;
+  }
+  public set budget(value: number) {
+    this._budget = value;
+  }
+  public get childId(): string {
+    return this._childId;
+  }
+  public set childId(value: string) {
+    this._childId = value;
+  }
+
+  //Constructor
+  constructor(actName: string, descrip: string, location: string, dayOfWeek: string, timeSlot: string, budget: number, childId: string)
+  {
+    this._activityName=actName;
+    this._description = descrip;
+    this._location = location;
+    this._dayOfTheWeek = dayOfWeek;
+    this._timeSlot = timeSlot;
+    this._budget = budget;
+    this._childId = childId;
+  }
+}
