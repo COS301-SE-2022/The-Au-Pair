14509b57f226a688f2dd706dc9ae6785195be326
Kyle_Pottinger <93247114+PxttytheThxtty@users.noreply.github.com>
2022-05-11 22:10:26 +0200

    Removed comments and redundant imports

--- libs/parent-dashboard/feature/src/lib/parent-dashboard.ts
+++ libs/parent-dashboard/feature/src/lib/parent-dashboard.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-// import { Router } from '@angular/router';
-// import { ShellRoutingModule } from '../../../../shell/feature/src/lib/shell-routing.module';
  
 @Component({
   selector: 'the-au-pair-parent-dashboard',
