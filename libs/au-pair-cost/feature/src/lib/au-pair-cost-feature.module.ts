3c48e1566e59f002ed0c1c46e5bce15925982c42
Reece Stephenson <u20431997@tuks.co.za>
2022-05-12 11:51:53 +0200

    Backend Graph Implementation

--- libs/au-pair-cost/feature/src/lib/au-pair-cost-feature.module.ts
+++ libs/au-pair-cost/feature/src/lib/au-pair-cost-feature.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { AuPairCostComponent } from './au-pair-cost.component';
 import { AuPairCostRoutingModule } from './au-pair-cost-routing.module';
 import { IonicModule } from '@ionic/angular';
+import { API } from '../../../../shared/api/api.service';
 
 @NgModule({
   imports: [CommonModule ,AuPairCostRoutingModule, IonicModule],
   declarations: [AuPairCostComponent],
+  providers: [API]
 })
 export class AuPairCostFeatureModule {}
--- libs/au-pair-cost/feature/src/lib/au-pair-cost.component.ts
+++ libs/au-pair-cost/feature/src/lib/au-pair-cost.component.ts
@@ -1,12 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { API } from '../../../../shared/api/api.service'
 
 @Component({
   selector: 'the-au-pair-au-pair-cost',
   templateUrl: './au-pair-cost.component.html',
   styleUrls: ['./au-pair-cost.component.scss'],
+  providers: [API]
 })
-export class AuPairCostComponent {
+export class AuPairCostComponent implements OnInit {
+
+  constructor(private api:API) { }
+
   days = [
     "Mon","Tue","Wed","Thu","Fri","Sat","Sun"
-  ]
+  ];
+
+  auPairName = "";
+  hourlyRate = 0;
+  totalHours = 0;
+  totalRemuneration = 0;
+
+  travelCost = 0;
+  activityCost = 0;
+  otherCost = 180;
+  totalCost = 0;
+
+  otherDeg = 0;
+  activityDeg = 0;
+
+  pieSplit = "conic-gradient(var(--ion-color-primary)" + this.otherDeg + "deg, var(--ion-color-secondary) 0 "+ this.activityDeg +"deg, var(--ion-color-champagne) 0)";
+
+  ngOnInit() { 
+    // this.api.getUser().subscribe( 
+    //   data => { 
+    //     this.auPairName = data.name
+    //   },
+    //   error => {
+    //     console.error();
+    //   }
+    // )
+
+    this.api.getAuPair().subscribe( 
+      data => { 
+        this.hourlyRate = data.payRate;
+        this.totalHours = data.hoursWorked;
+
+        this.travelCost = data.distTraveled;
+        this.activityCost = data.costIncurred;
+        this.otherCost = 180; 
+
+        this.totalCost = this.travelCost+this.activityCost+this.otherCost;
+        this.totalCost = Number(this.totalCost.toFixed(3))
+
+        this.totalRemuneration = (this.hourlyRate*this.totalHours) + this.totalCost; 
+        this.calculatePie(this.travelCost, this.activityCost, this.otherCost, this.totalCost);
+        this.pieSplit = "conic-gradient(var(--ion-color-primary)" + this.otherDeg + "deg, var(--ion-color-secondary) 0 "+ this.activityDeg +"deg, var(--ion-color-champagne) 0)";
+      },
+      error => {
+        console.error();
+      }
+    )
+    
+  }
+
+  calculatePie(dist:number, act:number, other:number, total:number) {
+    this.otherDeg = (360/total)*other;
+    this.activityDeg = this.otherDeg + (360/total)*act;
+  }
+
 }
