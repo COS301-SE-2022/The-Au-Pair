7140647ba0c68a85689c5eb10f95ab895b8f3b6b
priyolan <priyolan.pillay@gmail.com>
2022-05-12 19:31:27 +0200

    ✅(Tests) : Unit testing for backend

---
+++ Server/src/test/java/Database/TheAuPair/ParentRepositoryTests.java
@@ -0,0 +1,35 @@
+package Database.TheAuPair;
+
+import Database.TheAuPair.Models.Parent;
+import Database.TheAuPair.Repositories.ParentRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class ParentRepositoryTests
+{
+  @Autowired
+  ParentRepository  pr;
+
+  String id;
+  Parent expectedParent;
+
+  @BeforeEach
+  public void setup()
+  {
+    String [] c = new String[2];
+    expectedParent = new Parent("",c,"","");
+    id = "4561237814867";
+  }
+
+  @Test
+  @DisplayName("Test if parent object is returned")
+  public void testGetParentById()
+  {
+    assertEquals(pr.findUsingId(id).getClass(),expectedParent.getClass());
+  }
+}
---
+++ Server/src/test/java/Database/TheAuPair/auPairRepositoryTests.java
@@ -0,0 +1,35 @@
+package Database.TheAuPair;
+
+import Database.TheAuPair.Models.auPair;
+import Database.TheAuPair.Repositories.auPairRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class auPairRepositoryTests
+{
+  @Autowired
+  auPairRepository apr;
+
+  String id;
+  auPair expectedAuPair;
+
+  @BeforeEach
+  public void setup()
+  {
+    double [] week = {1,2,3,4,5,6,7};
+    expectedAuPair = new auPair("",0,0,0,0,true,"","",week);
+    id = "7542108615984";
+  }
+
+  @Test
+  @DisplayName("Test if auPair object is returned")
+  public void testGetAuPairById()
+  {
+    assertEquals(apr.findUsingId(id).getClass(),expectedAuPair.getClass());
+  }
+}
