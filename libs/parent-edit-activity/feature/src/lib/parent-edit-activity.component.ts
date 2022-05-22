3725b7351a097e4b5c63dd0ea34d224325434b94
Daniel Azmanov <u20441135@tuks.co.za>
2022-05-22 14:34:51 +0200

    removed place holder text and added labels

--- libs/parent-edit-activity/feature/src/lib/parent-edit-activity.component.ts
+++ libs/parent-edit-activity/feature/src/lib/parent-edit-activity.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { API } from '../../../../shared/api/api.service';
+import { Activity } from '../../../../shared/interfaces/activity.interfaces';
 
 @Component({
   selector: 'the-au-pair-parent-edit-activity',
@@ -6,13 +8,187 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./parent-edit-activity.component.scss'],
 })
 export class ParentEditActivityComponent implements OnInit {
-  constructor() {
-    console.log();
-    
+   //Activity Model
+   activityDetails: Activity = {
+    id: "",
+    name: "",
+    description: "",
+    location: "",
+    timeStart: "",
+    timeEnd: "",
+    budget: 0.0,
+    comment: "",
+    behavior: "",
+    day: "",
+    child: "",
+  };
+
+  //Children of logged in user
+  allChildren: any;
+
+  //Constructor
+  constructor(private serv: API) {}
+
+  ngOnInit(): void 
+  {
+    //Call getChildren service
+    this.getChildren();
   }
 
-  ngOnInit(): void {
-    console.log();
+  //Populate the activityDetails object from form input
+  async getActivityValues(val : any)
+  {  
+    //FORM ERROR CHECKING
+    let emptyInput = false;
+    let dom = document.getElementById("actNameError");
+    if(val.activityName === "")
+    {
+      emptyInput = true;
+      if(dom != null)
+      {
+        dom.innerHTML = "Activity name is empty";
+        dom.style.display = "block";
+      }
+    }
+    else
+    {
+      if(dom != null)
+      {
+        dom.style.display = "none";
+      }
+    }
+    dom = document.getElementById("descripError");
+    if(val.description === "")
+    { 
+      emptyInput = true;
+      if(dom != null)
+      {
+        dom.innerHTML = "Description is empty";
+        dom.style.display = "block";
+      }
+    }else
+    {
+      if(dom != null)
+      {
+        dom.style.display = "none";
+      }
+    }
+    dom = document.getElementById("locError");
+    if(val.location === "")
+    {
+      emptyInput = true;
+      if(dom != null)
+      {
+        dom.innerHTML = "Location is empty";
+        dom.style.display = "block";
+      }
+    }else
+    {
+      if(dom != null)
+      {
+        dom.style.display = "none";
+      }
+    }
+    dom = document.getElementById("dayError");
+    if(val.dayOfWeek === "")
+    {
+      emptyInput = true;
+      if(dom != null)
+      {
+        dom.innerHTML = "Day of the week is empty";
+        dom.style.display = "block";
+      }
+    }else
+    {
+      if(dom != null)
+      {
+        dom.style.display = "none";
+      }
+    }
+    dom = document.getElementById("timeError");
+    if(val.timeSlot === "" || val.timeSlot.length < 11)
+    {
+      emptyInput = true;
+      if(dom != null)
+      {
+        dom.innerHTML = "Time slot is empty";
+        dom.style.display = "block";
+      }
+    }else
+    {
+      if(dom != null)
+      {
+        dom.style.display = "none";
+      }
+    }
+    dom = document.getElementById("budgetError");
+    if(val.budget === "")
+    {
+      emptyInput = true;
+      if(dom != null)
+      {
+        dom.innerHTML = "Budget is empty";
+        dom.style.display = "block";
+      }
+    }else
+    {
+      if(dom != null)
+      {
+        dom.style.display = "none";
+      }
+    }
+    dom = document.getElementById("childError");
+    if(val.childId === "")
+    {
+      emptyInput = true;
+      if(dom != null)
+      {
+        dom.innerHTML = "Child ID is empty";
+        dom.style.display = "block";
+      }
+    }
+    else
+    {
+      if(dom != null)
+      {
+        dom.style.display = "none";
+      }
+    }
     
+    if(emptyInput == true)
+    {
+      console.log("You cannot add an activity with empty fields.");
+    }
+    else
+    {
+      const budget = parseInt(val.budget);
+      this.activityDetails.name = val.activityName;
+      this.activityDetails.description = val.description;
+      this.activityDetails.location = val.location;
+      this.activityDetails.day = val.dayOfWeek;
+      this.activityDetails.timeStart = val.timeSlot.substring(0,5);
+      this.activityDetails.timeEnd = val.timeSlot.substring(6,11);
+      this.activityDetails.budget = budget;
+      this.activityDetails.child = val.childId;
+      this.addActivity(this.activityDetails);
+    }
+  }
+
+  //Service calls
+  addActivity(act:Activity){
+    this.serv.addActivity(act).subscribe(
+      res=>{location.reload()},
+      error=>{console.log("Error has occured with API");}
+    )
+  };
+
+  getChildren()
+  {
+    this.serv.getParent().subscribe(
+      res=>{
+          this.allChildren = res.children;
+      },
+      error=>{console.log("Error has occured with API");}
+    )
   }
 }
