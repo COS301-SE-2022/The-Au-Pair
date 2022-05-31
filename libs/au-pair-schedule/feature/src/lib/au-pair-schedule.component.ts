0c7f4d0fdac9ab38c33d611375abc9c9818257ad
Ruben <u20463163@tuks.co.za>
2022-05-31 08:35:33 +0200

    Basic setup

--- libs/au-pair-schedule/feature/src/lib/au-pair-schedule.component.ts
+++ libs/au-pair-schedule/feature/src/lib/au-pair-schedule.component.ts
@@ -1,10 +1,39 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { API } from '../../../../shared/api/api.service';
 
 @Component({
   selector: 'the-au-pair-au-pair-schedule',
   templateUrl: './au-pair-schedule.component.html',
   styleUrls: ['./au-pair-schedule.component.scss'],
 })
-export class AuPairScheduleComponent  {
+export class AuPairScheduleComponent implements OnInit {
+  days = [
+    "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"
+  ]
 
+  curDay =  this.getCurDay(this.days);
+  activities: any;
+
+  constructor(private serv: API) {}
+
+  ngOnInit(): void {
+      this.getActivities();
+  }
+
+  getCurDay(days : string[]) : number {
+    const pipe = new DatePipe('en-US');
+    const dateStr = pipe.transform(Date.now(),'EEEE');
+    return days.findIndex(x => x === dateStr);
+  }
+
+  async getActivities()
+  {
+    this.serv.getSchedule().subscribe(
+      res=>{
+          this.activities = res;
+      },
+      error=>{console.log("Error has occured with API: " + error);}
+    )
+  }
 }
--- libs/au-pair-schedule/feature/src/lib/au-pair-schedule.module.ts
+++ libs/au-pair-schedule/feature/src/lib/au-pair-schedule.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuPairScheduleComponent } from './au-pair-schedule.component';
 import { AuPairScheduleRoutingModule } from './au-pair-schedule-routing.module';
+import { NavbarModule } from '@the-au-pair/shared/components/navbar';
+import { IonicModule } from '@ionic/angular';
+import { API } from '../../../../shared/api/api.service';
 
 @NgModule({
-  imports: [CommonModule,AuPairScheduleRoutingModule],
+  imports: [CommonModule,AuPairScheduleRoutingModule, NavbarModule, IonicModule],
   declarations: [AuPairScheduleComponent],
+  providers: [API]
 })
-export class AuPairScheduleModule {}
+export class AuPairScheduleModule {
+  
+}
