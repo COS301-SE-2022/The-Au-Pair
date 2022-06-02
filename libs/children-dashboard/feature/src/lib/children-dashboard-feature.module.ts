c1724596e203e7ca7f0d300e6a6d0bda97d747c6
Daniel Azmanov <u20441135@tuks.co.za>
2022-06-02 17:52:08 +0200

    Static page for children dashboard

--- libs/children-dashboard/feature/src/lib/children-dashboard-feature.module.ts
+++ libs/children-dashboard/feature/src/lib/children-dashboard-feature.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChildrenDashboardComponent } from './children-dashboard.component';
 import { ChildrenDashboardRoutingModule } from './children-dashboard-routing.module';
+import { IonicModule } from '@ionic/angular';
+import { NavbarModule } from '@the-au-pair/shared/components/navbar';
 
 @NgModule({
   imports: [
     CommonModule,
-    ChildrenDashboardRoutingModule
+    ChildrenDashboardRoutingModule,
+    IonicModule,
+    NavbarModule
   ],
   declarations: [ChildrenDashboardComponent],
 })
