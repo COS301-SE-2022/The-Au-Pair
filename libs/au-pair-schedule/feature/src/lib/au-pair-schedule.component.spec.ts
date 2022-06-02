8da99281710e37354e3b8a02470b692fc55a3ff7
Ruben <u20463163@tuks.co.za>
2022-06-02 19:34:19 +0200

    Testing fixes

--- libs/au-pair-schedule/feature/src/lib/au-pair-schedule.component.spec.ts
+++ libs/au-pair-schedule/feature/src/lib/au-pair-schedule.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { IonicModule, ModalController } from '@ionic/angular';
+import { API } from '../../../../shared/api/api.service';
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { RouterTestingModule} from '@angular/router/testing';
 import { AuPairScheduleComponent } from './au-pair-schedule.component';
+import { CommonModule } from '@angular/common';
+import { NavbarModule } from '@the-au-pair/shared/components/navbar';
 
 describe('AuPairScheduleComponent', () => {
   let component: AuPairScheduleComponent;
@@ -9,6 +14,8 @@ describe('AuPairScheduleComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AuPairScheduleComponent],
+      imports: [IonicModule, CommonModule,HttpClientTestingModule,NavbarModule, RouterTestingModule],
+      providers: [API,ModalController]
     }).compileComponents();
   });
 
--- libs/au-pair-schedule/feature/src/lib/schedule-modal/schedule-modal.component.spec.ts
+++ libs/au-pair-schedule/feature/src/lib/schedule-modal/schedule-modal.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { IonicModule, ModalController } from '@ionic/angular';
+import { API } from '../../../../../shared/api/api.service';
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { RouterTestingModule} from '@angular/router/testing';
 import { ScheduleModalComponent } from './schedule-modal.component';
+import { CommonModule } from '@angular/common';
+import { NavbarModule } from '@the-au-pair/shared/components/navbar';
 
 describe('ScheduleModalComponent', () => {
   let component: ScheduleModalComponent;
@@ -9,6 +14,8 @@ describe('ScheduleModalComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ScheduleModalComponent],
+      imports: [IonicModule, CommonModule,HttpClientTestingModule,NavbarModule, RouterTestingModule],
+      providers: [API,ModalController]
     }).compileComponents();
   });
 
