3bf612332713c45d78eed7675a01ec7ea2b8f58f
Kyle_Pottinger <93247114+PxttytheThxtty@users.noreply.github.com>
2022-06-04 15:56:20 +0200

    Added Parent Details

--- libs/parent-profile/feature/src/lib/parent-profile-feature.module.ts
+++ libs/parent-profile/feature/src/lib/parent-profile-feature.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ParentProfileComponent } from './parent-profile.component';
 import { ParentProfileRoutingModule } from './parent-profile-routing.module';
+import { IonicModule } from '@ionic/angular';
+import { NavbarModule } from '@the-au-pair/shared/components/navbar';
 
 @NgModule({
-  imports: [CommonModule, ParentProfileRoutingModule],
+  imports: [CommonModule, ParentProfileRoutingModule, IonicModule, NavbarModule],
   declarations: [ParentProfileComponent],
 })
 export class ParentProfileFeatureModule {}
