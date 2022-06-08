31f23bf9cebd6d579344efade3df02921ef90b49
Reece <r.james.stephenson@gmail.com>
2022-06-08 02:44:36 +0200

    💄Added togglable button for hours logging

--- libs/au-pair-dashboard/feature/src/lib/au-pair-dashboard.component.ts
+++ libs/au-pair-dashboard/feature/src/lib/au-pair-dashboard.component.ts
@@ -1,8 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { API } from '../../../../shared/api/api.service'
 
 @Component({
   selector: 'the-au-pair-au-pair-dashboard',
   templateUrl: './au-pair-dashboard.component.html',
   styleUrls: ['./au-pair-dashboard.component.scss'],
+  providers: [API]
 })
-export class AuPairDashboardComponent  {}
+export class AuPairDashboardComponent  {
+  constructor(private api:API) { }
+
+  buttonVisible = false;
+
+
+  logSwitch() {
+    this.buttonVisible = !this.buttonVisible;
+  }
+}
