d29f9f2d7d7b63fa1afd26205b55988af052b962
Kyle_Pottinger <93247114+PxttytheThxtty@users.noreply.github.com>
2022-06-09 11:48:38 +0200

    Updated Parent Profile spec.ts

--- libs/parent-profile/feature/src/lib/parent-profile.component.spec.ts
+++ libs/parent-profile/feature/src/lib/parent-profile.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ParentProfileComponent } from './parent-profile.component';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { NavbarModule } from '@the-au-pair/shared/components/navbar';
+import { RouterTestingModule} from '@angular/router/testing';
+import { API } from '../../../../shared/api/api.service';
 
 describe('ParentProfileComponent', () => {
   let component: ParentProfileComponent;
@@ -9,6 +14,19 @@ describe('ParentProfileComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ParentProfileComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ParentProfileComponent],
+      imports: [FormsModule,
+        IonicModule,
+        HttpClientTestingModule,
+        NavbarModule,
+        RouterTestingModule
+       ],
+       providers:[API]
     }).compileComponents();
   });
 
