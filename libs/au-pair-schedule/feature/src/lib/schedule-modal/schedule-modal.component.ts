0d6398c938cc55e5cc8aa183966ef654f96d30c0
Ruben <u20463163@tuks.co.za>
2022-06-09 17:06:01 +0200

    Added forms

--- libs/au-pair-schedule/feature/src/lib/schedule-modal/schedule-modal.component.ts
+++ libs/au-pair-schedule/feature/src/lib/schedule-modal/schedule-modal.component.ts
@@ -9,9 +9,21 @@ import { API } from '../../../../../shared/api/api.service';
   styleUrls: ['./schedule-modal.component.scss'],
 })
 export class ScheduleModalComponent {
+  
+  activityComment!: string;
+  activityBehaviour! : number;
+
   constructor(private serv: API,private modalCtrl : ModalController) {}
 
   closeModal(){
     this.modalCtrl.dismiss();
   }
+
+  getDescription(formData : any){
+    this.activityComment = formData.desc;
+    this.activityBehaviour = formData.behaviour;
+    
+  }
+
+
 }
