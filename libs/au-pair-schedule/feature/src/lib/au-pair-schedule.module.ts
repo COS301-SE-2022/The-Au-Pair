86f714a3e4bf8e4faf42fc43ee1e0d32ae65d4cc
Ruben <u20463163@tuks.co.za>
2022-06-09 19:36:45 +0200

    au pair navbar updates

--- libs/au-pair-schedule/feature/src/lib/au-pair-schedule.module.ts
+++ libs/au-pair-schedule/feature/src/lib/au-pair-schedule.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuPairScheduleComponent } from './au-pair-schedule.component';
 import { AuPairScheduleRoutingModule } from './au-pair-schedule-routing.module';
-import { NavbarModule } from '@the-au-pair/shared/components/navbar';
+import { AuPairNavbarModule } from '@the-au-pair/shared/components/aupair-navbar';
 import { IonicModule } from '@ionic/angular';
 import { API } from '../../../../shared/api/api.service';
 import { ScheduleModalComponent } from './schedule-modal/schedule-modal.component';
@@ -12,7 +12,7 @@ import { FormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     AuPairScheduleRoutingModule,
-    NavbarModule,
+    AuPairNavbarModule,
     IonicModule,
     FormsModule,
   ],
