46e365550027e71f73a0d591fffae27b7e203558
Daniel Azmanov <u20441135@tuks.co.za>
2022-06-10 08:32:21 +0200

    Dynamic population of fields

--- libs/parent-view-activity/feature/src/lib/parent-view-activity.component.ts
+++ libs/parent-view-activity/feature/src/lib/parent-view-activity.component.ts
@@ -8,12 +8,68 @@ import { Activity } from '../../../../shared/interfaces/interfaces';
   templateUrl: './parent-view-activity.component.html',
   styleUrls: ['./parent-view-activity.component.scss'],
 })
-export class ParentViewActivityComponent implements OnInit {
-  constructor(private serv: API, private router: Router) {}
+export class ParentViewActivityComponent implements OnInit
+{
+  //------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  /**Variables**/
 
-  ngOnInit(): void {
-    console.log();
+   //Activity Model
+   activityDetails: Activity = {
+    id: "",
+    name: "",
+    description: "",
+    location: "",
+    timeStart: "",
+    timeEnd: "",
+    budget: 0.0,
+    comment: "",
+    behavior: 0,
+    day: "",
+    child: "",
+  };
+  timeslot = "";
+  constructor(private serv: API, private router: Router)
+  {
+    //Receive activity ID from schedule page
+    const navigation = this.router.getCurrentNavigation();
+    if(navigation !== null)
+      if(navigation.extras !== null)
+      { 
+        this.activityDetails.id = navigation.extras.state?.['id'];
+      }
   }
+
+  ngOnInit(): void
+  {
+    this.getActivityDetails();
+  }
+
+  //------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  /**Service calls**/
+  getActivityDetails()
+  { 
+    this.serv.getActivity(this.activityDetails.id).subscribe(
+      res=>{
+        console.log("The response is:" + res); 
+        this.activityDetails.id = res.id;
+        this.activityDetails.name = res.name;
+        this.activityDetails.description = res.description;
+        this.activityDetails.location = res.location;
+        this.activityDetails.day = res.day;
+        this.activityDetails.timeStart = res.timeStart;
+        this.activityDetails.timeEnd = res.timeEnd;
+        this.activityDetails.behavior = res.behavior;
+        this.activityDetails.comment = res.comment;
+        this.activityDetails.budget = res.budget;
+        this.activityDetails.child = res.child;
+        this.timeslot = res.timeStart + "-" + res.timeEnd
+      },
+      error=>{console.log("Error has occured with API: " + error);}
+    )
+  };
+
+  //------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  /**Navigation**/
 
   returnToSchedule()
   {
