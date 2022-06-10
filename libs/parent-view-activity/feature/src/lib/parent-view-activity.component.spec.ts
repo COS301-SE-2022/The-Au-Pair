6b0223c876deb6f939b48497bcca13d078daa9f7
Daniel Azmanov <u20441135@tuks.co.za>
2022-06-10 09:02:01 +0200

    Updated testing to fix errors

--- libs/parent-view-activity/feature/src/lib/parent-view-activity.component.spec.ts
+++ libs/parent-view-activity/feature/src/lib/parent-view-activity.component.spec.ts
@@ -3,7 +3,6 @@ import { ParentViewActivityComponent } from './parent-view-activity.component';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { API } from '../../../../shared/api/api.service';
-import { Activity } from '../../../../shared/interfaces/interfaces';
 import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { NavbarModule } from '@the-au-pair/shared/components/navbar';
 import { RouterTestingModule } from "@angular/router/testing";
