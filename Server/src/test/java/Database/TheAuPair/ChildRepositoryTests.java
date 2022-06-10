8f611dff1c7551ea484297c8313a2ce34ea5e1d9
Daniel Azmanov <u20441135@tuks.co.za>
2022-06-10 12:40:10 +0200

    Hot fix for back-end test

--- Server/src/test/java/Database/TheAuPair/ChildRepositoryTests.java
+++ Server/src/test/java/Database/TheAuPair/ChildRepositoryTests.java
@@ -1,7 +1,7 @@
 package Database.TheAuPair;
 
 import Database.TheAuPair.Models.Child;
-import Database.TheAuPair.Repositories.ChildRepository;;
+import Database.TheAuPair.Repositories.ChildRepository;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
