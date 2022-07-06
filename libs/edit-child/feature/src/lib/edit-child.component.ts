1900015a58b833f04f1fb2fda092bda7eff9a5cb
Daniel Azmanov <u20441135@tuks.co.za>
2022-07-06 19:45:28 +0200

    Stlying and static page done

--- libs/edit-child/feature/src/lib/edit-child.component.ts
+++ libs/edit-child/feature/src/lib/edit-child.component.ts
@@ -31,7 +31,15 @@ export class EditChildComponent implements OnInit {
   //Regex for south african ID number
   SA_ID = new RegExp(/(((\d{2}((0[13578]|1[02])(0[1-9]|[12]\d|3[01])|(0[13456789]|1[012])(0[1-9]|[12]\d|30)|02(0[1-9]|1\d|2[0-8])))|([02468][048]|[13579][26])0229))(( |-)(\d{4})( |-)(\d{3})|(\d{7}))/);
 
-  constructor(private serv: API, public router: Router, public toastCtrl: ToastController) {}
+  constructor(private serv: API, public router: Router, public toastCtrl: ToastController)
+  {
+    const navigation = this.router.getCurrentNavigation();
+    if(navigation !== null)
+      if(navigation.extras !== null)
+      { 
+        this.childDetails.id = navigation.extras.state?.['id'];
+      }
+  }
 
   ngOnInit(): void {
     console.log();
