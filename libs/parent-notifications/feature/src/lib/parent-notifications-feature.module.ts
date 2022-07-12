a18d882d544aa9077fdb24afa74410e4e20338c5
Ruben <u20463163@tuks.co.za>
2022-07-12 18:14:17 +0200

    Static design

--- libs/parent-notifications/feature/src/lib/parent-notifications-feature.module.ts
+++ libs/parent-notifications/feature/src/lib/parent-notifications-feature.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, Route } from '@angular/router';
+import { Route } from '@angular/router';
+import { ParentNotificationsComponent } from './parent-notifications.component';
+import { ParentNotificationsRoutingModule } from './parent-notifications-routing.module';
+import { NavbarModule } from '@the-au-pair/shared/components/navbar';
+import { IonicModule } from '@ionic/angular';
 
 export const parentNotificationsFeatureRoutes: Route[] = [];
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, ParentNotificationsRoutingModule,NavbarModule,IonicModule],
+  declarations: [ParentNotificationsComponent],
 })
 export class ParentNotificationsFeatureModule {}
