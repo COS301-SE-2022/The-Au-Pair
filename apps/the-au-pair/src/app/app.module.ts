1a82fbd7d78f0ccd2ad278f43509a28c5f64aa80
priyolan <priyolan.pillay@gmail.com>
2022-07-14 17:36:22 +0200

    NGXS Install
    Installed NGXS and imported it to the app

--- apps/the-au-pair/src/app/app.module.ts
+++ apps/the-au-pair/src/app/app.module.ts
@@ -4,11 +4,20 @@ import { NavbarModule } from '@the-au-pair/shared/components/navbar';
 import { ShellModule } from '@the-au-pair/shell/feature';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NgxsModule } from '@ngxs/store';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [IonicModule.forRoot(), ShellModule, HttpClientModule, NavbarModule],
+  imports: [
+    IonicModule.forRoot(), 
+    ShellModule, 
+    HttpClientModule, 
+    NavbarModule,
+    NgxsModule.forRoot([
+      
+    ]),
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
