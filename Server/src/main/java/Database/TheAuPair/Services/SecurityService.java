62ddec5b402ea2dd49dd4afe15b8a3a93a292169
priyolan <priyolan.pillay@gmail.com>
2022-07-15 15:12:19 +0200

    Security Initialization
    Added dependencies and service for security

---
+++ Server/src/main/java/Database/TheAuPair/Services/SecurityService.java
@@ -0,0 +1,12 @@
+package Database.TheAuPair.Services;
+
+import org.bouncycastle.crypto.generators.Argon2BytesGenerator;
+import org.bouncycastle.crypto.params.Argon2Parameters;
+
+import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+public class SecurityService
+{
+}
