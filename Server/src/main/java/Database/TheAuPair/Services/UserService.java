aac7e31950810cc69682e17746325a58d247ee7f
priyolan <priyolan.pillay@gmail.com>
2022-07-15 15:15:02 +0200

    Added Functions for UserService
    Added Register and Login functions

--- Server/src/main/java/Database/TheAuPair/Services/UserService.java
+++ Server/src/main/java/Database/TheAuPair/Services/UserService.java
@@ -22,4 +22,49 @@ public class UserService
   {
     ur.save(u);
   }
+
+  public String register(User u)
+  {
+    for (User registered : ur.findAll())
+    {
+      if (registered.getId().equals(u.getId()) || registered.getEmail().equals(u.getEmail()))
+      {
+        return registered.getEmail();
+      }
+    }
+
+    SecurityService SS = new  SecurityService();
+    String salt = SS.genSalt().toString();
+    String hash = SS.genPassword(u.getPassword(), salt.getBytes());
+
+    u.setPassword(hash);
+    u.setSalt(salt);
+    ur.save(u);
+    return "pending";
+  }
+
+  public String login(String email, String password)
+  {
+    for (User registered : ur.findAll())
+    {
+      if (registered.getEmail().equals(email))
+      {
+        if (registered.isRegistered())
+        {
+          SecurityService SS = new SecurityService();
+          String hash = SS.genPassword(password, registered.getSalt().getBytes());
+
+          if (hash.equals(registered.getPassword()))
+          {
+            return registered.getId();
+          }
+        }
+        else
+        {
+          return "pending";
+        }
+      }
+    }
+    return "";
+  }
 }
