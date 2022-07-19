82497535deaff7d3071ac4b1e3ed7aec4c255e95
Reece <r.james.stephenson@gmail.com>
2022-07-19 12:32:38 +0200

    💄Added Admin Navbar Component

--- libs/admin-console/feature/src/lib/admin-console-feature.module.ts
+++ libs/admin-console/feature/src/lib/admin-console-feature.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminConsoleComponent } from './admin-console.component';
+import { AdminConsoleRoutingModule } from './admin-console-routing.module';
+import { AdminNavbarModule } from '@the-au-pair/shared/components/admin-navbar';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, AdminConsoleRoutingModule, AdminNavbarModule],
   declarations: [AdminConsoleComponent],
 })
 export class AdminConsoleFeatureModule {}
