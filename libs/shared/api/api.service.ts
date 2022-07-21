4116a2dfee481bd48eefd4b8af7c2ac93754c1dd
priyolan <priyolan.pillay@gmail.com>
2022-07-21 10:50:23 +0200

    Added Service to API
    Added resolveApplication Service

--- libs/shared/api/api.service.ts
+++ libs/shared/api/api.service.ts
@@ -158,4 +158,28 @@ export class API{
   removeAuPair(id : string): Observable<any> {
     return this.http.post('http://localhost:8080/removeAuPair',id);
   }
+
+  resolveApplication(id : string, resolution : boolean): Observable<any> {
+    var decision = {
+      "id" : id,
+      "resolution" : ""
+    }
+    if(resolution)
+    {
+      decision.resolution = "approve";
+    }
+    else
+    {
+      decision.resolution = "decline";
+      this.removeAuPair(id).subscribe(
+        res => {
+          console.log("The response is:" + res); 
+        },
+        error => {
+          console.log("Error has occured with API: " + error);
+        }
+      );
+    }
+    return this.http.post('http://localhost:8080/resolveApplication',decision);
+  }
 }
