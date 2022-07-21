2e6b6bf9e1b2911dac9f1a7f56d6412e8934f479
priyolan <priyolan.pillay@gmail.com>
2022-07-21 21:21:09 +0200

    register API service redone
    simplified register API call

--- libs/shared/api/api.service.ts
+++ libs/shared/api/api.service.ts
@@ -127,6 +127,11 @@ export class API{
   updateHoursLog(hl : HoursLogged): Observable<any> {
     return this.http.post("http://localhost:8080/updateHoursLog", hl);
   }
+
+  register(user : User): Observable<any>  {
+    return this.http.post('http://localhost:8080/register',user, {responseType: 'text'})
+  }
+  
   registerParent(user : User, parent : Parent):  Observable<any>  {
     return this.http.post('http://localhost:8080/register',user, {responseType: 'text'})
   }
