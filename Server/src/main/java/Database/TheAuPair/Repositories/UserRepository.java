669610fe8d9ea2babf371dc5b20bdb536f4a4439
priyolan <priyolan.pillay@gmail.com>
2022-07-21 10:14:07 +0200

    Added UserRepository function
    added function top find all users with a specific type

--- Server/src/main/java/Database/TheAuPair/Repositories/UserRepository.java
+++ Server/src/main/java/Database/TheAuPair/Repositories/UserRepository.java
@@ -5,10 +5,15 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.mongodb.repository.Query;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends MongoRepository<User, String>
 {
   @Query("{_id:'?0'}")
   User findUsingId(String id);
+
+  @Query("{ 'type': ?0 }")
+  List<User> findAllByType(int type);
 }
 
