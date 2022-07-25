6b17ec0ad36fd6d2949812b73691619c25883c36
priyolan <priyolan.pillay@gmail.com>
2022-07-25 11:03:01 +0200

    Parent dashboard NGXS implementation
    updated routing and API calls to use store

--- libs/parent-dashboard/feature/src/lib/parent-dashboard.ts
+++ libs/parent-dashboard/feature/src/lib/parent-dashboard.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { API } from '../../../../shared/api/api.service';
 import { auPair, Child, Parent, User } from '../../../../shared/interfaces/interfaces';
 import { AuPairRatingModalComponent } from './au-pair-rating-modal/au-pair-rating-modal.component';
-import { ModalController } from '@ionic/angular';
+import { ModalController, ToastController } from '@ionic/angular';
+import { Store } from '@ngxs/store';
+import { AppState, AppStateModel } from 'libs/shared/ngxs/state';
+import { Navigate } from 'libs/shared/ngxs/actions';
 
 @Component({
   selector: 'the-au-pair-parent-dashboard',
@@ -55,7 +58,7 @@ export class ParentDashboardComponent implements OnInit{
     experience: "",
   }
 
-  constructor(private serv: API, private modalCtrl : ModalController){}
+  constructor(private serv: API, private modalCtrl : ModalController, public store : Store, public toastCtrl : ToastController){}
 
   async openModal(actId : string) {
     const modal = await this.modalCtrl.create({
@@ -69,12 +72,13 @@ export class ParentDashboardComponent implements OnInit{
 
   ngOnInit(): void
   {
+    this.parentID = this.store.snapshot().user.id;
     this.getParentDetails()
   }
 
   async getParentDetails()
   {
-    await this.serv.getParent("4561237814867").subscribe(
+    await this.serv.getParent(this.parentID).subscribe(
       res=>{
         this.parentDetails.id = res.id;       
         this.parentID = res.id;
@@ -84,41 +88,47 @@ export class ParentDashboardComponent implements OnInit{
       },
       error=>{console.log("Error has occured with API: " + error);}
     )
-    await this.serv.getUser("7542108615984").subscribe(
-      res=>{
-        this.userDetails.id = res.id;
-        this.userDetails.fname = res.fname;
-        this.userDetails.sname = res.sname;
-        this.userDetails.email = res.email;
-        this.userDetails.address = res.address;
-        this.userDetails.registered = res.registered;
-        this.userDetails.type = res.type;
-        this.userDetails.password = res.password;
-        this.userDetails.number = res.number;
-        this.userDetails.salt = res.salt;
-      },
-      error=>{console.log("Error has occured with API: " + error);}
-    )
-    await this.serv.getAuPair("7542108615984").subscribe(
-      res=>{
-        this.auPairDetails.id = res.id;
-        this.auPairDetails.rating = res.rating;
-        this.auPairDetails.onShift = res.onShift;
-        this.auPairDetails.employer = res.employer;
-        this.auPairDetails.costIncurred = res.costIncurred;
-        this.auPairDetails.distTraveled = res.distTraveled;
-        this.auPairDetails.payRate = res.payRate;
-        this.auPairDetails.bio = res.bio;
-        this.auPairDetails.experience = res.experience;
-      },
-      error=>{console.log("Error has occured with API: " + error);}
-    )
+    if(this.parentDetails.auPair != "")
+    {
+      await this.serv.getUser(this.parentID).subscribe(
+        res=>{
+          this.userDetails.id = res.id;
+          this.userDetails.fname = res.fname;
+          this.userDetails.sname = res.sname;
+          this.userDetails.email = res.email;
+          this.userDetails.address = res.address;
+          this.userDetails.registered = res.registered;
+          this.userDetails.type = res.type;
+          this.userDetails.password = res.password;
+          this.userDetails.number = res.number;
+          this.userDetails.salt = res.salt;
+        },
+        error=>{console.log("Error has occured with API: " + error);}
+      )
 
-    this.getChildren()
+      await this.serv.getAuPair(this.parentDetails.auPair).subscribe(
+        res=>{
+          this.auPairDetails.id = res.id;
+          this.auPairDetails.rating = res.rating;
+          this.auPairDetails.onShift = res.onShift;
+          this.auPairDetails.employer = res.employer;
+          this.auPairDetails.costIncurred = res.costIncurred;
+          this.auPairDetails.distTraveled = res.distTraveled;
+          this.auPairDetails.payRate = res.payRate;
+          this.auPairDetails.bio = res.bio;
+          this.auPairDetails.experience = res.experience;
+        },
+        error=>{console.log("Error has occured with API: " + error);}
+      )
+    }
+    if(this.parentDetails.children.length > 0)
+    {
+      this.getChildren()
+    }
   }
 
   async getChildren(){
-    this.serv.getChildren("4561237814867").subscribe(
+    this.serv.getChildren(this.parentID).subscribe(
       res=>{
         let i = 0;
         res.forEach((element: string) => {
@@ -131,4 +141,20 @@ export class ParentDashboardComponent implements OnInit{
     )
   }
 
+  nav(path: string) 
+  {
+    this.store.dispatch(new Navigate(path));
+  }
+
+  async openToast(message: string)
+  {
+    const toast = await this.toastCtrl.create({
+      message: message,
+      duration: 4000,
+      position: 'top',
+      color: 'primary',
+      cssClass: 'toastPopUp'
+    });
+    await toast.present();
+  }
 }
