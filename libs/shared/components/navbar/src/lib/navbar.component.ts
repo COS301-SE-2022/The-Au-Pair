2eb4247623c3d12b169c464b058b84811e612101
priyolan <priyolan.pillay@gmail.com>
2022-07-26 23:20:23 +0200

    fixed routes in navbar

--- libs/shared/components/navbar/src/lib/navbar.component.ts
+++ libs/shared/components/navbar/src/lib/navbar.component.ts
@@ -32,7 +32,7 @@ export class NavbarComponent implements OnInit
     }
     else if(this.type == 2)
     {
-      this.router.navigate(['/aupair-dashboard']);
+      this.router.navigate(['/au-pair-dashboard']);
     }
   }
 
@@ -64,7 +64,7 @@ export class NavbarComponent implements OnInit
     }
     else if(this.type == 2)
     {
-      this.router.navigate(['/aupair-dashboard']);
+      this.router.navigate(['/au-pair-dashboard']);
     }
   }
 
