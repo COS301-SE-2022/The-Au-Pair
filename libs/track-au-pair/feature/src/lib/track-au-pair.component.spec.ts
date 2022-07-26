8be88780c9cb64baa7e2af8cb8e30f76818d3d50
Daniel Azmanov <u20441135@tuks.co.za>
2022-07-27 00:04:49 +0200

    Added imports and providers for tests

--- libs/track-au-pair/feature/src/lib/track-au-pair.component.spec.ts
+++ libs/track-au-pair/feature/src/lib/track-au-pair.component.spec.ts
@@ -1,5 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { API } from '../../../../shared/api/api.service';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { NavbarModule } from '@the-au-pair/shared/components/navbar';
+import { RouterTestingModule} from '@angular/router/testing';
+import { NgxsModule } from '@ngxs/store';
+import { AppState } from '../../../../shared/ngxs/state';
 import { TrackAuPairComponent } from './track-au-pair.component';
 
 describe('TrackAuPairComponent', () => {
@@ -9,6 +16,14 @@ describe('TrackAuPairComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [TrackAuPairComponent],
+      imports: [FormsModule,
+        IonicModule,
+        HttpClientTestingModule,
+        NavbarModule,
+        RouterTestingModule,
+        NgxsModule.forRoot([AppState]),
+        ],
+        providers: [API]
     }).compileComponents();
   });
 
