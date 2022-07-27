8a3551727c59dc46c303d7861f8e15d6031cd0dd
ricecakesreece <r.james.stephenson@gmail.com>
2022-07-27 09:46:39 +0200

    💄Reports page setup

--- libs/admin-reports/feature/src/lib/admin-reports-feature.module.ts
+++ libs/admin-reports/feature/src/lib/admin-reports-feature.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminReportsComponent } from './admin-reports.component';
 import { AdminReportsRoutingModule } from './admin-reports-routing.module';
-import { AdminNavbarModule } from '@the-au-pair/shared/components/admin-navbar';
+import { NavbarModule } from '@the-au-pair/shared/components/navbar';
 import { IonicModule } from '@ionic/angular';
 import { API } from '../../../../shared/api/api.service';
 
 @NgModule({
-  imports: [CommonModule, AdminReportsRoutingModule, AdminNavbarModule, IonicModule],
+  imports: [CommonModule, AdminReportsRoutingModule, NavbarModule, IonicModule],
   declarations: [AdminReportsComponent],
   providers: [API]
 })
--- libs/admin-reports/feature/src/lib/admin-reports.component.ts
+++ libs/admin-reports/feature/src/lib/admin-reports.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { API } from "../../../../shared/api/api.service";
 
 @Component({
   selector: 'the-au-pair-admin-reports',
@@ -6,7 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-reports.component.scss'],
 })
 export class AdminReportsComponent implements OnInit {
-  constructor() {}
+  
+  auPairs : any[] = [];
 
-  ngOnInit(): void {}
+  constructor(private serv: API) {}
+
+  ngOnInit(): void {
+    this.getSignUpRequests();
+  }
+
+  getSignUpRequests() {
+    this.serv.getApplicants().toPromise().then(res => {
+      this.auPairs = res;
+      console.log(this.auPairs);
+    }).catch(err => {
+      console.log(err);
+    });
+  }
+
+  resolve(userId : string, choice : boolean) {
+    this.serv.resolveApplication(userId,choice).toPromise().then(res => {
+      window.location.reload();
+      return choice;
+    }).catch(err => {
+      console.log(err);
+    });
+  }
+
 }
