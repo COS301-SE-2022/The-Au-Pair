2482646d67c8d7d0d4cc4aa2cd5cde6180c086ea
ricecakesreece <r.james.stephenson@gmail.com>
2022-07-28 11:13:03 +0200

    Update broken API calls and Age in explore modal

--- libs/explore/feature/src/lib/expand-modal/expand-modal.component.ts
+++ libs/explore/feature/src/lib/expand-modal/expand-modal.component.ts
@@ -91,4 +91,16 @@ export class ExpandModalComponent implements OnInit {
       error=>{console.log("Error has occured with API: " + error);}
     )
   }
+
+  getAge(dateString : string) {
+    const today = new Date();
+    const birthDate = new Date(dateString);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+
+    return age;
+  }
 }
