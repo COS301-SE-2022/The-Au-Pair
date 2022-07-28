86ef2757d47aeb3f58c108d43c72e6eb5ba86414
ricecakesreece <r.james.stephenson@gmail.com>
2022-07-28 21:14:54 +0200

    ✅ Fixed test

--- libs/admin-reports/feature/src/lib/admin-reports.component.spec.ts
+++ libs/admin-reports/feature/src/lib/admin-reports.component.spec.ts
@@ -1,5 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AdminReportsComponent } from './admin-reports.component';
+import { IonicModule } from '@ionic/angular';
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { RouterTestingModule} from '@angular/router/testing';
+import { API } from '../../../../shared/api/api.service';
+import { NavbarModule } from '@the-au-pair/shared/components/navbar';
+import { NgxsModule } from '@ngxs/store';
+import { AppState } from '../../../../shared/ngxs/state';
+
 
 describe('AdminReportsComponent', () => {
   let component: AdminReportsComponent;
@@ -8,6 +16,8 @@ describe('AdminReportsComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AdminReportsComponent],
+      imports: [IonicModule,HttpClientTestingModule,RouterTestingModule,NavbarModule,NgxsModule.forRoot([AppState])],
+      providers: [API]
     }).compileComponents();
   });
 
