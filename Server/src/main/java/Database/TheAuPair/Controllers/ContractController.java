a97c9fe012448a25257d1e8487b629ca85d998e9
Kyle_Pottinger <93247114+PxttytheThxtty@users.noreply.github.com>
2022-07-28 19:50:57 +0200

    Added HTTP framing and Bind errors

--- Server/src/main/java/Database/TheAuPair/Controllers/ContractController.java
+++ Server/src/main/java/Database/TheAuPair/Controllers/ContractController.java
@@ -4,6 +4,8 @@ import Database.TheAuPair.Models.Contract;
 import Database.TheAuPair.Repositories.ContractRepository;
 import Database.TheAuPair.Services.ContractService;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.BindingResult;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 import java.util.Map;
@@ -20,45 +22,100 @@ public class ContractController
 
   @PostMapping("/getContract")
   @CrossOrigin(origins = "http://localhost:4200")
-  public Contract getContract(@RequestBody String id)
+  public ResponseEntity<Contract> getContract(@RequestBody String id, BindingResult bindingResult)
   {
-    Contract ct =  cts.getContract(id);
-    return ct;
+    if (bindingResult.hasErrors())
+    {
+      return ResponseEntity
+        .badRequest()
+        .body(null);
+    }
+    else
+    {
+      return ResponseEntity
+        .ok()
+        .body(this.cts.getContract(id));
+    }
   }
 
   @PostMapping("/getContractbyIDs")
   @CrossOrigin(origins = "http://localhost:4200")
-  public Contract getContractbyIDs(@RequestBody Map<String, String> IDs)
+  public ResponseEntity<Contract> getContractbyIDs(@RequestBody Map<String, String> IDs, BindingResult bindingResult)
   {
-    Contract ct = cts.getContractbyIDs(IDs.get("parentID"), IDs.get("auPairID"));
-    return ct;
+    if (bindingResult.hasErrors())
+    {
+      return ResponseEntity
+        .badRequest()
+        .body(null);
+    }
+    else
+    {
+      return ResponseEntity
+        .ok()
+        .body(this.cts.getContractbyIDs(IDs.get("parentID"), IDs.get("auPairID")));
+    }
   }
 
   @PostMapping("/editContract")
   @CrossOrigin(origins = "http://localhost:4200")
-  public void editContract(@RequestBody Contract c)
+  public ResponseEntity.BodyBuilder editContract(@RequestBody Contract c, BindingResult bindingResult)
   {
-    this.cts.updateContract(c);
+    if (bindingResult.hasErrors())
+    {
+      return (ResponseEntity.BodyBuilder) ResponseEntity
+        .badRequest()
+        .body(null);
+    }
+    else
+    {
+      this.cts.updateContract(c);
+      return ResponseEntity
+        .ok();
+    }
   }
 
   @PostMapping("/removeContract")
   @CrossOrigin(origins = "http://localhost:4200")
-  public void removeContract(@RequestBody String id)
+  public ResponseEntity.BodyBuilder removeContract(@RequestBody String id, BindingResult bindingResult)
   {
-    this.cts.deleteContract(id);
+    if (bindingResult.hasErrors())
+    {
+      return (ResponseEntity.BodyBuilder) ResponseEntity
+        .badRequest()
+        .body(null);
+    }
+    else
+    {
+      this.cts.deleteContract(id);
+      return ResponseEntity
+        .ok();
+    }
   }
 
   @PostMapping("/addContract")
   @CrossOrigin(origins = "http://localhost:4200")
-  public void addContract(@RequestBody Contract c)
+  public ResponseEntity.BodyBuilder addContract(@RequestBody Contract c, BindingResult bindingResult)
   {
-    this.cts.addContract(c);
+    if (bindingResult.hasErrors())
+    {
+      return (ResponseEntity.BodyBuilder) ResponseEntity
+        .badRequest()
+        .body(null);
+    }
+    else
+    {
+      this.cts.addContract(c);
+      return ResponseEntity
+        .ok();
+    }
   }
 
   @GetMapping("/getAllContracts")
   @CrossOrigin(origins = "http://localhost:4200")
-  public List<Contract> getAllContracts()
+  public ResponseEntity<List<Contract>> getAllContracts()
   {
-    return this.cts.getAllContracts();
+    return ResponseEntity
+        .ok()
+        .body(this.cts.getAllContracts());
   }
 }
