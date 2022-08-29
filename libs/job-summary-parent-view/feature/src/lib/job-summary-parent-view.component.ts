a8e6eb6f045fb12f43fb097b0f90ab2a7e9b7a4a
Kyle_Pottinger <93247114+PxttytheThxtty@users.noreply.github.com>
2022-08-29 20:12:38 +0200

    Created Static Job Summary Page

--- libs/job-summary-parent-view/feature/src/lib/job-summary-parent-view.component.ts
+++ libs/job-summary-parent-view/feature/src/lib/job-summary-parent-view.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { API } from '../../../../shared/api/api.service';
 import { Store } from '@ngxs/store';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'the-au-pair-job-summary-parent-view',
@@ -8,7 +9,75 @@ import { Store } from '@ngxs/store';
   styleUrls: ['./job-summary-parent-view.component.scss'],
 })
 export class JobSummaryParentViewComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit(): void {}
+/* eslint-disable @typescript-eslint/no-explicit-any */
+  aupairID = "";
+
+  days = [
+    "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"
+  ]
+
+  curDay =  this.getCurDay(this.days);
+  auPairChildren: string[] = [];
+  activities: any;
+  children : any;
+  childActivity : any = {
+    childName : "",
+    childId : "",
+    activityName : "",
+    activityId : "",
+    time : "",
+    dayofweek : "",
+  }
+  childActivities : any[] = [];
+
+  constructor(private serv: API, private store: Store) {}
+
+  ngOnInit(): void {
+      this.aupairID = this.store.snapshot().user.id;
+      this.getActivities();
+  }
+
+  getCurDay(days : string[]) : number {
+    const pipe = new DatePipe('en-US');
+    const dateStr = pipe.transform(Date.now(),'EEEE');
+    return days.findIndex(x => x === dateStr);
+  }
+
+  async getActivities(){
+    console.log("getActivities");
+    this.serv.getChildren(this.aupairID).subscribe(
+      res => {
+        this.children = res;
+        this.children.forEach((element: { id: string; }) => {
+          this.auPairChildren.push(element.id);
+        });
+        this.serv.getAuPairSchedule(this.auPairChildren).subscribe(
+          res=>{
+            this.activities = res;
+            this.setChildActivity();
+          }
+        );
+      },
+      error => { console.log("Error has occured with API: " + error); },
+    );
+  }
+
+  setChildActivity(){
+    this.children.forEach((child: { id: any; fname: any; }) => {
+      this.activities.forEach((act: { childId: any; child: any; name: any; id: any; timeStart: any; day: any; }) => {
+        if(child.id === act.child){
+          const childActivity = {
+            childName : child.fname,
+            childId : act.child,
+            activityName : act.name,
+            activityId : act.id,
+            time : act.timeStart,
+            dayofweek : act.day,
+          }
+          this.childActivities.push(childActivity);
+        }
+      });
+    });
+  }
 }
