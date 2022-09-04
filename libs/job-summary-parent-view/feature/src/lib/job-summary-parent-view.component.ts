666d0d317f20c6bf18508b954a326b700ef7c425
Kyle_Pottinger <93247114+PxttytheThxtty@users.noreply.github.com>
2022-09-04 15:54:50 +0200

    Completed Job Summary Parent View

--- libs/job-summary-parent-view/feature/src/lib/job-summary-parent-view.component.ts
+++ libs/job-summary-parent-view/feature/src/lib/job-summary-parent-view.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { API } from '../../../../shared/api/api.service';
 import { Store } from '@ngxs/store';
 import { DatePipe } from '@angular/common';
+import { Child, Parent, User } from 'libs/shared/interfaces/interfaces';
 
 @Component({
   selector: 'the-au-pair-job-summary-parent-view',
@@ -11,7 +12,8 @@ import { DatePipe } from '@angular/common';
 export class JobSummaryParentViewComponent implements OnInit {
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
-  aupairID = "";
+  parentID = "";
+  childrenArr: Child[] = [];
 
   days = [
     "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"
@@ -31,11 +33,99 @@ export class JobSummaryParentViewComponent implements OnInit {
   }
   childActivities : any[] = [];
 
+  childDetails: Child ={
+    id: "",
+    fname: "",
+    sname: "",
+    allergies: "",
+    diet: "",
+    parent: "",
+    aupair: "",
+  }
+
+  parentDetails: Parent = {
+    id: "",
+    children: [],
+    medID: "",
+    auPair: "",
+  }
+
+  userDetails: User = {
+    id: '',
+    fname: '',
+    sname: '',
+    email: '',
+    address: '',
+    registered: false,
+    type: 0,
+    password: '',
+    number: '',
+    salt: '',
+    latitude: 0,
+    longitude: 0,
+    suburb: "",
+    gender: "",
+    fcmToken : "",
+    birth: "",
+    warnings: 0,
+    banned: "",
+  }
+
   constructor(private serv: API, private store: Store) {}
 
-  ngOnInit(): void {
-      this.aupairID = this.store.snapshot().user.id;
-      this.getActivities();
+  async ngOnInit(): Promise<void> {
+    this.parentID = this.store.snapshot().user.id;
+    this.getActivities();
+
+    
+    await this.serv.getUser(this.parentID).toPromise()
+    .then( 
+      res=>{
+        this.userDetails.id = res.id;
+        this.userDetails.fname = res.fname;
+        this.userDetails.sname = res.sname;
+        this.userDetails.email = res.email;
+        this.userDetails.address = res.address;
+        this.userDetails.number = res.number;
+        this.userDetails.salt = res.salt;
+        this.userDetails.latitude = res.latitude;
+        this.userDetails.longitude = res.longitude;
+        this.userDetails.suburb = res.suburb;
+        this.userDetails.gender = res.gender;
+        this.userDetails.birth = res.birth;
+        this.userDetails.warnings = res.warnings;
+        this.userDetails.banned = res.banned;
+      },
+      error => {
+        console.log("Error has occured with API: " + error);
+      }
+    )
+    
+    await this.serv.getParent(this.parentID)
+    .toPromise()
+      .then( 
+        res=>{
+          this.parentDetails.id = res.id;      
+          this.parentID = res.id;
+          this.parentDetails.children = res.children;
+          this.parentDetails.medID = res.medID;
+          this.parentDetails.auPair = res.auPair;
+      },
+      error => {
+        console.log("Error has occured with API: " + error);
+      }
+    )
+
+    
+    await this.serv.getChildren(this.parentID).subscribe(
+        res=>{
+          let i = 0;
+          res.forEach((element: Child) => {
+            this.childrenArr[i++] = element;
+          });
+        },
+        error =>{console.log("Error has occured with API: " + error);}
+      )
   }
 
   getCurDay(days : string[]) : number {
@@ -46,7 +136,7 @@ export class JobSummaryParentViewComponent implements OnInit {
 
   async getActivities(){
     console.log("getActivities");
-    this.serv.getChildren(this.aupairID).subscribe(
+    this.serv.getChildren(this.parentID).subscribe(
       res => {
         this.children = res;
         this.children.forEach((element: { id: string; }) => {
