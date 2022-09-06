000fa6efa2fab521bef62f71042d98acc190fdda
priyolan <priyolan.pillay@gmail.com>
2022-09-07 00:55:33 +0200

    Updated Controller CORS
    Allow access from anywhere

--- Server/src/main/java/Database/TheAuPair/Controllers/ChildController.java
+++ Server/src/main/java/Database/TheAuPair/Controllers/ChildController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class ChildController
 {
   private ChildService cs;
@@ -17,28 +18,24 @@ public class ChildController
   }
 
   @PostMapping("/getChildren")
-  @CrossOrigin(origins = "http://localhost:4200")
   public List<Child> getChildren(@RequestBody String id)
   {
     return this.cs.getChildren(id);
   }
 
   @PostMapping("/addChild")
-  @CrossOrigin(origins = "http://localhost:4200")
   public void addChild(@RequestBody Child c)
   {
     this.cs.addChild(c);
   }
 
   @PostMapping("/updateChild")
-  @CrossOrigin(origins = "http://localhost:4200")
   public void updateChild(@RequestBody Child c)
   {
     this.cs.updateChild(c);
   }
 
   @PostMapping("/removeChild")
-  @CrossOrigin(origins = "http://localhost:4200")
   public void removeChild(@RequestBody String id)
   {
     this.cs.removeChild(id);
