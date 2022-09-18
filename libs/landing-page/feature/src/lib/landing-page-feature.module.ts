d1ed0d5aa100e4e9826af0e13d33008385b8f0bc
ricecakesreece <r.james.stephenson@gmail.com>
2022-09-18 23:48:14 +0200

    💄Added carousels to landing page

--- libs/landing-page/feature/src/lib/landing-page-feature.module.ts
+++ libs/landing-page/feature/src/lib/landing-page-feature.module.ts
@@ -4,9 +4,10 @@ import { LandingPageComponent } from './landing-page.component';
 import { LandingPageComponentRoutingModule } from './landing-page-routing.module';
 import { NavbarModule } from '@the-au-pair/shared/components/navbar';
 import { IonicModule } from '@ionic/angular';
+import { SwiperModule } from 'swiper/angular';
 
 @NgModule({
-  imports: [CommonModule, LandingPageComponentRoutingModule, NavbarModule, IonicModule],
+  imports: [CommonModule, LandingPageComponentRoutingModule, NavbarModule, IonicModule, SwiperModule],
   declarations: [LandingPageComponent],
 })
 export class LandingPageFeatureModule {}
--- libs/landing-page/feature/src/lib/landing-page.component.ts
+++ libs/landing-page/feature/src/lib/landing-page.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import SwiperCore, { Autoplay, Swiper } from 'swiper';
+
+SwiperCore.use([Autoplay]);
 
 @Component({
   selector: 'the-au-pair-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss'],
 })
-export class LandingPageComponent {
+export class LandingPageComponent{
+
+  @ViewChild('parentSwiper') parentSwiper: any;
+  @ViewChild('auPairSwiper') auPairSwiper: any;
+
+  constructor() {}
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    this.parentSwiper.swiperRef.autoplay.start();
+    this.auPairSwiper.swiperRef.autoplay.start();
+  }
+
+  slideOpts = {
+    speed: 400,
+    loop: true,
+    spaceBetween: 20,
+  };
 
   scrollToParent() {
     document.getElementById('section-parent')?.scrollIntoView({
@@ -19,5 +40,27 @@ export class LandingPageComponent {
       behavior: 'smooth',
       block: 'start'
     });
+  }
+
+  setSwiperInstance(swiper: Swiper) {
+    setInterval(() => {
+      swiper.slideNext();
+    }, 6000);
+  }
+
+  nextParentSlide() {
+    this.parentSwiper?.swiperRef.slideNext(500);
+  }
+
+  previousParentSlide() {
+    this.parentSwiper?.swiperRef.slidePrev(500);
+  }
+
+  nextAuPairSlide() {
+    this.auPairSwiper?.swiperRef.slideNext(500);
+  }
+
+  previousAuPairSlide() {
+    this.auPairSwiper?.swiperRef.slidePrev(500);
   }
 }
