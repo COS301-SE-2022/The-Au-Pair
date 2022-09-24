edd33a66b9cbb330a580e3d20540c74f8094ef75
priyolan <priyolan.pillay@gmail.com>
2022-09-24 15:47:02 +0200

    Added Function for downloading files

--- Server/src/main/java/Database/TheAuPair/Services/StorageService.java
+++ Server/src/main/java/Database/TheAuPair/Services/StorageService.java
@@ -1,7 +1,12 @@
 package Database.TheAuPair.Services;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.util.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
+
+import java.io.IOException;
 
 public class StorageService
 {
@@ -12,5 +17,21 @@ public class StorageService
   public StorageService(AmazonS3 s3)
   {
     this.s3 = s3;
+  }
+
+  public byte[] downloadFile(String fileName)
+  {
+    S3Object s3Object = s3.getObject(bucketName, fileName);
+    S3ObjectInputStream inputStream = s3Object.getObjectContent();
+    try
+    {
+      byte[] content = IOUtils.toByteArray(inputStream);
+      return content;
+    }
+    catch (IOException e)
+    {
+      e.printStackTrace();
+    }
+    return null;
   }
 }
