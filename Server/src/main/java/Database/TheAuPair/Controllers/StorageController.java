988360ef2900e2d5abb9c42dc0c5946ae9c06490
priyolan <priyolan.pillay@gmail.com>
2022-09-24 15:59:14 +0200

    Added Endpoint for file download

--- Server/src/main/java/Database/TheAuPair/Controllers/StorageController.java
+++ Server/src/main/java/Database/TheAuPair/Controllers/StorageController.java
@@ -2,6 +2,8 @@ package Database.TheAuPair.Controllers;
 
 import Database.TheAuPair.Services.StorageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -16,5 +18,18 @@ public class StorageController
   public String upload(@RequestParam("file") MultipartFile file)
   {
     return ss.uploadFile(file);
+  }
+
+  @PostMapping("/getFile")
+  public ResponseEntity<ByteArrayResource> downloadFile(@RequestBody String filename)
+  {
+    byte[] data = ss.downloadFile(filename);
+    ByteArrayResource resource = new ByteArrayResource(data);
+    return ResponseEntity
+      .ok()
+      .contentLength(data.length)
+      .header("Content-type", "application/octet-stream")
+      .header("Content-disposition", "attachment; filename=\"" + filename + "\"")
+      .body(resource);
   }
 }
