19246a02f0bdfdba0d3bce1128cdd3894d6117cd
Kyle_Pottinger <93247114+PxttytheThxtty@users.noreply.github.com>
2022-09-28 10:29:30 +0200

    Changed Child ID Number to Child Age

--- libs/parent-dashboard/feature/src/lib/parent-dashboard.ts
+++ libs/parent-dashboard/feature/src/lib/parent-dashboard.ts
@@ -496,4 +496,16 @@ export class ParentDashboardComponent implements OnInit{
         }]});
     await alert.present();
   }
+
+  getAge(dateString : string) {
+    const today = new Date();
+    const birthDate = new Date(dateString);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+
+    return age;
+  }
 }
