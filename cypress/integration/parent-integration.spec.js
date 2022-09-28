5284d4879877b5437015c9e0585719a482780102
priyolan <priyolan.pillay@gmail.com>
2022-09-28 16:36:02 +0200

    Updated parent cypress tests

--- cypress/integration/parent-integration.spec.js
+++ cypress/integration/parent-integration.spec.js
@@ -4,8 +4,8 @@ describe('Parent integration tests', () => {});
 
 beforeEach( () => {
     cy.visit("/login-page")
-    cy.get(`[ng-reflect-name="Email"]`).type("bob@gmail.com"); 
-    cy.get(`[ng-reflect-name="Password"]`).type("Bob@1234"); 
+    cy.get(`[ng-reflect-name="Email"]`).type("mockparent@gmail.com"); 
+    cy.get(`[ng-reflect-name="Password"]`).type("Parent@123"); 
     cy.get(".loginUser").click();
 });
 
@@ -18,7 +18,7 @@ it('should not add an activity with a location not returned by the API', () => {
         cy.get('[ng-reflect-name="dayOfWeek"]').select("Monday");
         cy.get('[ng-reflect-name="timeSlot"]').select("05:00-06:00");
         cy.get('[ng-reflect-name="budget"]').type("0");
-        cy.get('[ng-reflect-name="childId"]').select("Peter");
+        cy.get('[ng-reflect-name="childId"]').select("Child 1");
         cy.get('.addActivity').click({force:true}).then( () => {
             cy.contains('Please select a valid location from the suggested below.');
         });
@@ -35,14 +35,13 @@ it('should load a map when attempting to track your au pairs live location', () 
 
 //Parent dashboard testing 
 it('should populate the parent dashboard with your registered children from the database', () => {
-    cy.contains("Peter");
-    cy.contains("Wally");
-    cy.contains("Sally");
+    cy.contains("Child 1");
+    cy.contains("Child 2");
 })
 
 it('should allow a parent to logout', () =>
 {
-    cy.get('.logout-button').click({force:true}).then( () => {
+    cy.get('.log-in-out-button').click({multiple:true, force:true}).then( () => {
         cy.contains("Login");
     });
 });
@@ -50,7 +49,7 @@ it('should allow a parent to logout', () =>
 // Parent Profile testing
 it('should show the logged in users name on the parent profile page', () => {
     cy.get("#profile").click({force:true}).then( () => {
-        cy.get('.profile-name').contains("Bob Stone");
+        cy.get('.profile-name').contains("Parent");
     });
 });
 
@@ -70,23 +69,22 @@ it('should show the logged in users name on the parent profile page', () => {
 // Schedule testing
 it('should populate the schedule with activities from the database', async () => {
     await cy.get("#schedule").click({force:true}).then( () => {
-        cy.get('.card').contains("AI Lesson");
+        cy.get('.card').contains("Mock Activity 3");
     });
 });
 
 // Children Testing 
 it('should populate the Children dashboard page with your registered children from the database', () => {
     cy.get("#childDash").click({force:true}).then( () => {
-        cy.contains("Peter Griffin");
-        cy.contains("Wally West");
-        cy.contains("Sally Siver");
+        cy.contains("Child 1 Mock");
+        cy.contains("Child 2 Mock");
     });
 })
 
 it('should allow a parent to route to "add a child" page', () => {
     cy.get("#childDash").click({force:true}).then( () => {
         cy.get('#addChild').click({force: true}).then( () => {
-            cy.contains("Add A Child");
+            cy.url().should('include', '/add-child') // => t
         })
     });
 })
@@ -94,7 +92,7 @@ it('should allow a parent to route to "add a child" page', () => {
 // Au Pair Cost Testing 
 it('should show the correct employed au pair when loading au pair cost', () => {
     cy.get("#auPairCost").click({force:true}).then( () => {
-        cy.get(".au-pair-name").contains("Denny");
+        cy.get(".au-pair-name").contains("Aupair");
     });
 })
 
