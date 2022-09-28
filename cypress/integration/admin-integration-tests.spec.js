742e831a9caa9afc8b400262ac3d3c0675eb68e6
priyolan <priyolan.pillay@gmail.com>
2022-09-28 13:11:21 +0200

    Updated admin cypress tests
    fixed name of buttons and parameters
    Updated descriptions

--- cypress/integration/admin-integration-tests.spec.js
+++ cypress/integration/admin-integration-tests.spec.js
@@ -4,27 +4,27 @@ describe('Admin integration tests', () => {});
 
 beforeEach( () => {
     cy.visit("/login-page")
-    cy.get(`[ng-reflect-name="Email"]`).type("irene@hotmail.za"); 
-    cy.get(`[ng-reflect-name="Password"]`).type("Ruben@123"); 
+    cy.get(`[ng-reflect-name="Email"]`).type("mockadmin@gmail.com"); 
+    cy.get(`[ng-reflect-name="Password"]`).type("Admin@123"); 
     cy.get(".loginUser").click();
 });
 
 //Logout testing
-it('should allow a Au Pair to logout', () =>
+it('should allow an Admin to logout', () =>
 {
-    cy.get('.logout-button').click({force:true}).then( () => {
+    cy.get('.log-in-out-button').click({multiple:true, force:true}).then( () => {
         cy.contains("Login");
     });
 });
 
-// Au Pair Profile testing
+//Admin permissin testing
 it('should not allow an admin to modify any details about the profile', () => {
     cy.get("#profile").click({force:true}).then( () => {
         cy.url().should('include', '/admin-console') // => true.
     });
 });
 
+//Au Pair Review testing
 it('should show cards of registration requests from the database (if any) upon loading the admin console', () =>{
     cy.get('.auPairCard').should('be.visible');
 });
-
