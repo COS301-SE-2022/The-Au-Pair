a36d307366ce4e04b37bf3166e02e8563b670297
priyolan <priyolan.pillay@gmail.com>
2022-09-28 14:14:21 +0200

    Added reports page testing

--- cypress/integration/admin-integration-tests.spec.js
+++ cypress/integration/admin-integration-tests.spec.js
@@ -9,7 +9,7 @@ beforeEach( () => {
     cy.get(".loginUser").click();
 });
 
-//Logout testing
+// Logout testing
 it('should allow an Admin to logout', () =>
 {
     cy.get('.log-in-out-button').click({multiple:true, force:true}).then( () => {
@@ -27,4 +27,24 @@ it('should not allow an admin to modify any details about the profile', () => {
 //Au Pair Review testing
 it('should show cards of registration requests from the database (if any) upon loading the admin console', () =>{
     cy.get('.auPairCard').should('be.visible');
+});
+
+it('should allow and Admin to go to the reports page', () =>{
+    cy.get(".reports-button").click().then( () => {
+        cy.url().should('include', '/admin-reports') // => true.
+    });
+});
+
+// Reports review testing
+it('should show cards of reports from the database (if any) upon naviagting to the reports page', () =>{
+    cy.get(".reports-button").click();
+    cy.get(".auPairCard").should('be.visible');
+});
+
+//Admin review actions testing
+it('should allow admins to ban, warn or dismiss reports on the repots page', () =>{
+    cy.get(".reports-button").click();
+    cy.get(".ban");
+    cy.get(".warn");
+    cy.get(".dismiss");
 });
